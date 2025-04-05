575fb350704ad96e99398c1436fee37ac66d7a17
Fedi-Nasri <fedinasri.fsb@gmail.com>
2025-04-05 15:05:17 +0100

    add Sensors

--- asv_wave_sim_gazebo/scripts/fan_controller.py
+++ asv_wave_sim_gazebo/scripts/fan_controller.py
@@ -4,6 +4,7 @@ import rospy
 from std_msgs.msg import Float64
 from gazebo_msgs.srv import ApplyBodyWrench, ApplyBodyWrenchRequest
 from gazebo_msgs.srv import BodyRequest
+from pynput import keyboard  # Import pynput for keyboard control
 
 class FanController:
     def __init__(self):
@@ -29,6 +30,17 @@ class FanController:
         rospy.loginfo("Publish Float64 to:")
         rospy.loginfo("- /fan_droit for right fan torque")
         rospy.loginfo("- /fan_gauche for left fan torque")
+         # Maximum torque limit
+        self.max_torque = 1.0
+        self.current_droit = 0.0
+        self.current_gauche = 0.0
+
+        rospy.loginfo("Fan controller initialized. Use Z/S/Q/D to move the boat.")
+        rospy.loginfo("Press 'Esc' to exit.")
+
+        # Start keyboard listener
+        self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
+        self.listener.start()
 
     def droit_callback(self, msg):
         """Handle right fan torque commands"""
@@ -58,6 +70,41 @@ class FanController:
                 self.apply_wrench(req)
         except rospy.ServiceException as e:
             rospy.logerr(f"Torque application failed for {link_name}: {str(e)}")
+    
+    
+    
+    
+    def on_press(self, key):
+        """Handle key press events"""
+        try:
+            if key.char == 's':  # Move forward
+                self.apply_torque("boatcleaningc::fandroit", 0.30)
+                self.apply_torque("boatcleaningc::fangauche", 0.30)
+            elif key.char == 'z':  # Move backward
+                self.apply_torque("boatcleaningc::fandroit", -1.30)
+                self.apply_torque("boatcleaningc::fangauche", -1.30)
+            elif key.char == 'd':  # Turn left (20% left, 80% right)
+                self.apply_torque("boatcleaningc::fandroit", -0.8)
+                self.apply_torque("boatcleaningc::fangauche", -0.2)
+            elif key.char == 'q':  # Turn right (80% left, 20% right)
+                self.apply_torque("boatcleaningc::fandroit", -0.2)
+                self.apply_torque("boatcleaningc::fangauche", -0.8)
+        except AttributeError:
+            pass  # Ignore non-character keys
+
+    def on_release(self, key):
+        """Stop movement when keys are released"""
+        self.apply_torque("boatcleaningc::fandroit", 0.0)
+        self.apply_torque("boatcleaningc::fangauche", 0.0)
+
+        # Exit program when 'Esc' is pressed
+        if key == keyboard.Key.esc:
+            rospy.signal_shutdown("User exited")
+            return False
+
+    def control_loop(self):
+        """Keep the ROS node running"""
+        rospy.spin()
 
 if __name__ == '__main__':
     controller = FanController()
